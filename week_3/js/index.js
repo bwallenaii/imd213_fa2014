a8a5a0aa89f963db99d0f3c85b41ab0653e73e7c
Brent Allen <bwallen.aii@gmail.com>
2014-10-23 03:54:36 +0000

    week 3 examples

---
+++ week_3/js/index.js
@@ -0,0 +1,46 @@
+document.addEvent("domready", function(){
+
+    //NAV START
+	var nav = document.id("main-nav");
+	var content = document.id("content");
+	var atags = nav.getElements("a");
+	var contentBlocks = content.getChildren("div");
+	
+	contentBlocks.set("reveal", {
+		duration: 3000,
+		transition: "bounce:out"
+	});
+
+	contentBlocks.setStyle("display", "none");
+	contentBlocks[0].setStyle("display");
+	
+	atags.addEvent("click", function(e){
+		e.stop();
+
+		//find the index
+		for(var i = 0; i < atags.length; ++i){
+			if(atags[i] == this)
+			{
+				break;
+			}
+
+		}
+		//hide all contentBlocks
+		contentBlocks.dissolve();
+		//show the contentBlocks that we want to keep
+		contentBlocks[i].reveal();
+		return false;
+	});
+	
+	//NAV END
+	
+	//FORM VALIDATION START
+	var checker = new FormCheck("new-member", {
+	    display:{
+	        indicateErrors: 2
+	    }
+	});
+	
+	
+	//FORM VALIDATION END
+});
