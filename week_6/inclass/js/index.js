a3b56332a7fb8de6a81690fed1a4ada5cacad5b1
Brent Allen <bwallen.aii@gmail.com>
2014-11-20 05:23:07 +0000

    week 7

--- week_6/inclass/js/index.js
+++ week_6/inclass/js/index.js
@@ -1,4 +1,23 @@
 document.addEvent("domready", function(){
-
-    new Window("navigator");
+    var colors = [
+        "#ffffff",
+        "#66cc33",
+        "#336699"
+    ];
+    var curColor = 0;
+   var window1 = new Window("navigator");
+   var window2 = new Window("navigator2", {
+        navSelector: "nav",
+        contentSelector:"ul",
+        contentElements:"li"
+   });
+   
+   window1.addEvent("update", function(e){
+       curColor++;
+       if(curColor >= colors.length)
+       {
+           curColor = 0;
+       }
+       document.getElement("body").setStyle("background-color", colors[e.index]);
+   });
 });
--- week_6/inclass/js/window.js
+++ week_6/inclass/js/window.js
@@ -1,6 +1,6 @@
 var Window = new Class({
 	
-	Implements: [Options],
+	Implements: [Options,Events],
 	
 	options:{
 	    navSelector:".navigation",
@@ -13,30 +13,35 @@ var Window = new Class({
 		    transition: "bounce:out"
 	    }
 	},
+	element: null,
+	navArea: null,
+	contentArea: null,
+	navElements: null,
+	contentElements: null,
 
 	initialize: function(element, options)
 	{
 		this.element = document.id(element);
 		this.setOptions(options);
-		
+		//collect my needed elements
 		this.navArea = this.element.getElement(this.options.navSelector);
 		this.contentArea = this.element.getElement(this.options.contentSelector);
 		this.navElements = this.navArea.getChildren(this.options.navElements);
 		this.contentElements = this.contentArea.getChildren(this.options.contentElements);
-		
+		//This makes sure everything will work (not necesserily normal)
 		if(this.navElements.length != this.contentElements.length)
 		{
 		    throw new Error("The number of navigation elements must match the number of content elements. Go fix your MiniDOM.");
 		}
-		
+		//add Event listeners
 		this.navElements.addEvent("click", this.navClicked.bind(this));
-		
+		//Set effects if we have them (not necesserily normal)
 		if(Fx.Reveal) //if we have the reveal effect
 		{
 		    this.contentElements.set("reveal", this.options.revealOptions);
 		    this.contentElements.setStyle("overflow", "hidden");
 		}
-		
+		//Show our first content item by default (not necesserily normal)
 		this.showContent(0, true);
 	},
 	
@@ -60,7 +65,7 @@ var Window = new Class({
 	{
 	    this.navElements.removeClass(this.options.selectedClass);
 	    this.navElements[index].addClass(this.options.selectedClass);
-	    
+	    this.fireEvent("update", {index:index});
 	    if(Fx.Reveal && !skipAnimation)
 	    {
 	        this.contentElements.dissolve();
